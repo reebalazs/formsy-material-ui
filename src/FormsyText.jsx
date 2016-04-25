0f133e43c027832dae06b23d4c584c8ae2ae8021
Balázs Reé <ree@greenfinity.hu>
2016-04-25 16:36:54 +0200

    Fix formsy text for correct handling of changes. Also allow changing defaultValues on the fly.

--- src/FormsyText.jsx
+++ src/FormsyText.jsx
@@ -3,9 +3,14 @@ import keycode from 'keycode';
 import Formsy from 'formsy-react';
 import TextField from 'material-ui/TextField';
 import {_setMuiComponentAndMaybeFocus} from './utils';
+import utils from 'formsy-react/src/utils.js';
+
+// Override mixin methods directly,
+// as doing this on the class would cause an invariant error.
+const { resetValue, componentWillReceiveProps, ...MixinFixed } = Formsy.Mixin;
 
 let FormsyText = React.createClass({
-  mixins: [ Formsy.Mixin ],
+  mixins: [MixinFixed],
 
   propTypes: {
     name: React.PropTypes.string.isRequired,
@@ -14,38 +19,80 @@ let FormsyText = React.createClass({
     onBlur: React.PropTypes.func
   },
 
-  handleBlur: function handleBlur(event) {
-    this.setValue(event.currentTarget.value);
-    if (this.props.onBlur) this.props.onBlur(event);
-  },
-
-  handleKeyDown: function handleKeyDown(event) {
-    if (keycode(event) === 'enter') this.handleEnterKeyDown(event);
-    if (this.props.onKeyDown) this.props.onKeyDown(event, event.currentTarget.value);
-  },
-  
-  handleEnterKeyDown: function handleEnterKeyDown(event) {
-   this.setValue(event.currentTarget.value);
-   if (this.props.onEnterKeyDown) this.props.onEnterKeyDown(event, event.currentTarget.value);
-  },
-
   _setMuiComponentAndMaybeFocus: _setMuiComponentAndMaybeFocus,
 
   render: function () {
-    // value must not be passed in, because it makes the field value unchangeable.
-    const { value, ...props } = this.props;
     return (
       <TextField
-        {...props}
+        {...this.props}
         ref={this._setMuiComponentAndMaybeFocus}
-        defaultValue={value}
-        onBlur={this.handleBlur}
-        onFocus={this.props.onFocus}
-        onKeyDown={this.handleKeyDown}
+        defaultValue={this.props.defaultValue}
+        value={this.getControlledValue()}
         errorText={this.getErrorMessage()}
+        onChange={evt => this.handleChange(evt)}
       />
     );
+  },
+
+  isControlledByCaller: function() {
+    return this.props.value !== undefined;
+  },
+
+  getControlledValue: function() {
+    if (this.isControlledByCaller()) {
+      // Controlled by caller: use the specified value.
+      return this.props.value;
+    } else {
+      const value = this.getValue();
+      if (this.isPristine()) {
+        // When pristine: duplicate defaultValue as value.
+        return this.props.defaultValue;
+      } else {
+        // value changed: do not set the value then.
+        return value;
+      }
+    }
+  },
+
+  handleChange: function(evt) {
+    const value = evt.target.value;
+    this.setValue(value);
+    if (this.props.onChange) {
+      this.props.onChange(evt);
+    }
+  },
+
+  // The next methods are to make sure that defaultValue can be changed.
+
+  componentWillReceiveProps: function (nextProps) {
+    const nextDefaultValue = nextProps.defaultValue;
+    if (nextDefaultValue !== undefined &&
+        nextDefaultValue !== this.props.defaultValue &&
+        this.isPristine()) {
+      // A defaultValue has changed and we are pristine.
+      // Save this as the new pristine state.
+      this.setState({
+        _pristineValue: nextDefaultValue,
+        _value: nextDefaultValue
+      });
+    }
+    // Do the original validation setting
+    this.setValidations(nextProps.validations, nextProps.required);
+  },
+
+  resetValue: function () {
+    // Must set it to the default value, instead of the pristine.
+    const defaultValue = this.props.defaultValue;
+    const pristineValue = defaultValue !== undefined ? defaultValue : this.state._pristineValue;
+    this.setState({
+      _value: pristineValue,
+      _pristineValue: pristineValue,
+      _isPristine: true
+    }, () => {
+      this.context.formsy.validate(this);
+    });
   }
+
 });
 
 export default  FormsyText;
